60c908af69102f9e4999ad5966e3fa46e327355e
luizh98 <dev.lhsantos@gmail.com>
2020-05-18 21:21:11 -0300

    Initial commit

---
+++ src/test/java/br/com/operacoes/OperacoesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.operacoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OperacoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
